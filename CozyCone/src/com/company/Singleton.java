aeb26ecd4991e5d2d4d0292c174e66cc492896b5
William Menegaldi <william.menegaldi@mrv.com.br>
2019-11-30 11:20:26 -0300

    singleton

---
+++ CozyCone/src/com/company/Singleton.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Singleton
+{
+    private static Singleton singleton;
+    private Singleton(){}
+
+    public static synchronized Singleton getInstance()
+    {
+        if (singleton == null)
+            singleton = new Singleton();
+        return singleton;
+    }
+}
